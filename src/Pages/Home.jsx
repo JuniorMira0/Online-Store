3961e6a2577a6962657f6a0125ab188294a6ad8a
JuniorMira <j-unyor14@hotmail.com>
2022-03-10 17:59:42 -0300

    requisito 3 feito por Junior/Lucas/Euclides

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,14 @@ export default class Home extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
+        <button type="submit" data-testid="shopping-cart-button">
+          <Link to="/shopping-cart" data-testid=""></Link>
+        </button>
+        <input
+          type="text"
+          placeholder="Pesquise um produto"
+          name="input-search"
+        />
         <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
       </div>
     );
