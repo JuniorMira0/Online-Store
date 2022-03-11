0dcb24e42dc69bc3c38a3fab6d849b3dcffa2a2a
Lucas Nascimento <lucasnascimento.lp@gmail.com>
2022-03-11 16:20:48 -0300

    Iniciando Requisito 5 com Murilo/ Euclides/ Luan/ Junior

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ShoppingCart from './ShoppingCart';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputProduct: '',
+    };
+  }
+
+  onChangeInput = ({target}) => {
+    const { value } = target;
+    this.setState ({
+      inputProduct: value
+    })
+  }
+
   render() {
+    const { inputProduct } = this.state;
     return (
       <div data-testid="home-initial-message">
+        <button type="submit" data-testid="shopping-cart-button">
+          <Link to="/shopping-cart"><ShoppingCart /></Link>
+        </button>
+        <input
+          type="text"
+          placeholder="Pesquise um produto"
+          name="input-search"
+          value={ inputProduct }
+          onChange={ this.onChangeInput }
+        />
         <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
       </div>
     );
---
+++ src/Pages/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    );
+  }
+}
+
+// Requisito feito por Junior/ Lucas/ Euclides
